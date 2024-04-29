6c163c2cf430f74c766999f688fbdcb33c13b927
David Alejandro Gómez <david@acapillatech.com.ar>
2024-04-29 16:42:17 -0300

    Ajuste de carga en includes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'david/wc-macro-click',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8eb17c82bc981fa264c671aea309939dd27295c7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'david/wc-macro-click' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8eb17c82bc981fa264c671aea309939dd27295c7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sha256encript/aes-encrypter' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b7a35005132f8ef1f8823e871ec614ce9c13d2ed',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sha256encript/aes-encrypter',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
