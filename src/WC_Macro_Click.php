bb104cf02a218e8c961917aee3615a0929c78d15
David Alejandro Gómez <david@acapillatech.com.ar>
2024-05-02 11:46:06 -0300

    Retorno al checkout luego de pago cancelado y/o fallido

--- src/WC_Macro_Click.php
+++ src/WC_Macro_Click.php
@@ -135,7 +135,7 @@ class WC_Macro_Click extends WC_Payment_Gateway {
       }
 
       $callback_success = $aes->EncryptString($this->get_return_url($order), $this->secret_key);
-      $callback_cancel = $aes->EncryptString($this->get_return_url($order), $this->secret_key);
+      $callback_cancel = $aes->EncryptString(wc_get_checkout_url(), $this->secret_key);
       $comercio = $this->id_comercio;
       $sucursal_comercio = $aes->EncryptString($this->sucursal_comercio, $this->secret_key);
       $transaccion_comercio_id = $order_id;
@@ -205,7 +205,7 @@ class WC_Macro_Click extends WC_Payment_Gateway {
          }
          
          if($status === '4' || $status === '7' || $status === '8' || $status === '11') {
-            $order->update_status('cancelled', 'Pedido cancelado por pago fallido');
+            $order->update_status('pending', 'Pedido en suspenso por pago fallido');
          }
       }
    }
